a506f379806a463673f7f193d3e1b4565842bf0e
hackingguy <akashchhabra710@gmail.com>
2021-03-26 12:01:42 +0530

    CORS Proxy

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const connectDB = require('./config/db');
 
 const indexRoute = require('./routes/index');
@@ -19,6 +20,7 @@ connectDB();
 
 var app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
